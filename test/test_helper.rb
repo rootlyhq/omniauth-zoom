a4164c0d84bc47dd12e2f48cc540a427f463d4c0
Quentin Rousseau <contact@quent.in>
2024-01-04 02:57:49 -0800

    simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,6 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 
 require 'simplecov'
 SimpleCov.start
-if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
 require 'minitest/autorun'
 require 'webmock/minitest'
 
